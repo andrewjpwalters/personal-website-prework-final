84ba3f5d2c7c54328f4c86a1339b261170e7b32e
Andrew Walters <andrewjpwalters@gmail.com>
2022-02-22 11:01:54 -0600

    Change JS functionality and add HTML content

--- script.js
+++ script.js
@@ -3,34 +3,41 @@
 
 // document.querySelector("body").appendChild(h2);
 
-const artifactBody = document.createElement('div');
+// const artifactBody = document.createElement('div');
 
-const artifactContainer = document.querySelector("#content");
+// const artifactContainer = document.querySelector("#content");
 
 const artifactOne = document.querySelector('#artifact-1');
 const artifactTwo = document.querySelector('#artifact-2');
 const artifactThree = document.querySelector('#artifact-3');
 
-artifactOne.addEventListener("click", function () {
-    const image = document.createElement('img');
-    image.src = "./images/house.jpeg";
-    artifactBody.append(image);
-    artifactContainer.appendChild(artifactBody);
-    return artifactContainer;
+artifactOne.addEventListener('click', function () {
+
 })
 
-artifactTwo.addEventListener("click", function () {
-    const image = document.createElement('img');
-    image.src = "./images/mannequin.jpeg";
-    artifactBody.append(image);
-    artifactContainer.appendChild(artifactBody);
-    return artifactContainer;
-})
+// artifactOne.addEventListener("click", function () {
+//     const image = document.createElement('img');
+//     const text = document.createElement('div');
+//     image.src = "./images/house.jpeg";
+//     artifactBody.append(image);
+//     artifactBody.appendChild(text);
+//     artifactContainer.appendChild(artifactBody);
+//     return artifactContainer;
+// })
 
-artifactThree.addEventListener("click", function () {
-    const image = document.createElement('img');
-    image.src = "./images/vines.jpeg";
-    artifactBody.append(image);
-    artifactContainer.appendChild(artifactBody);
-    return artifactContainer;
-})
+// artifactTwo.addEventListener("click", function () {
+//     const image = document.createElement('img');
+//     image.src = "./images/mannequin.jpeg";
+//     artifactBody.append(image);
+//     artifactContainer.appendChild(artifactBody);
+//     return artifactContainer;
+// })
+
+// artifactThree.addEventListener("click", function () {
+//     const image = document.createElement('img');
+//     image.src = "./images/vines.jpeg";
+//     artifactBody.append(image);
+//     artifactContainer.appendChild(artifactBody);
+//     return artifactContainer;
+// })
+
