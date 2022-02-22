0049eff3ee6e22319ac4e3749259165f535fee09
Andrew Walters <andrewjpwalters@gmail.com>
2022-02-22 10:36:18 -0600

    Add initial JS content and add additional HTML content

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+// document.querySelector("body").appendChild(h2);
+
+const artifactBody = document.createElement('div');
+
+const artifactContainer = document.querySelector("#content");
+
+const artifactOne = document.querySelector('#artifact-1');
+const artifactTwo = document.querySelector('#artifact-2');
+const artifactThree = document.querySelector('#artifact-3');
+
+artifactOne.addEventListener("click", function () {
+    const image = document.createElement('img');
+    image.src = "./images/house.jpeg";
+    artifactBody.append(image);
+    artifactContainer.appendChild(artifactBody);
+    return artifactContainer;
+})
+
+artifactTwo.addEventListener("click", function () {
+    const image = document.createElement('img');
+    image.src = "./images/mannequin.jpeg";
+    artifactBody.append(image);
+    artifactContainer.appendChild(artifactBody);
+    return artifactContainer;
+})
+
+artifactThree.addEventListener("click", function () {
+    const image = document.createElement('img');
+    image.src = "./images/vines.jpeg";
+    artifactBody.append(image);
+    artifactContainer.appendChild(artifactBody);
+    return artifactContainer;
+})
